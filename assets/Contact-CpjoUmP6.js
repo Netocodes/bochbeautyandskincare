2be36186dea9f848b4c562b83170b6662c4baf8a
Netochukwu Codes <161637474+Netocodes@users.noreply.github.com>
2024-12-27 19:26:39 +0100

    Updates

---
+++ assets/Contact-CpjoUmP6.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BDv7BIl5.js";const o=()=>t.jsx("div",{children:"ContactUs"});export{o as default};
---
+++ assets/productPage-C47qy5BJ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BDv7BIl5.js";const e=()=>r.jsx("div",{children:"ProductPage"});export{e as default};
