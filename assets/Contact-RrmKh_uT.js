7fc3b8b4063f569a40ebf7f3fc12015bde0c5e69
Netochukwu Codes <161637474+Netocodes@users.noreply.github.com>
2024-12-29 13:20:17 +0100

    Updates

---
+++ assets/Contact-RrmKh_uT.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Smvd8hNJ.js";const o=()=>t.jsx("div",{children:"ContactUs"});export{o as default};
---
+++ assets/productPage-DIQdTqpW.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Smvd8hNJ.js";const e=()=>r.jsx("div",{children:"ProductPage"});export{e as default};
