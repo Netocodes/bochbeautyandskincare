21f33ca2081be4e8c7545ccd9d7a2a5832b93a54
Netochukwu Codes <netochukwucodes@gmail.com>
2025-03-11 08:45:15 +0100

    Fix image source formatting in Herosection component

--- frontend/src/components/ppHerosection.tsx
+++ frontend/src/components/ppHerosection.tsx
@@ -6,8 +6,7 @@ const Herosection = () => {
           <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
             <img
               className="h-[17rem] w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-              src="https://res.cloudinary.com/de7tyskql/image/upload/f_auto,q_auto/zesz5qfddzmw0j1txpdj
-"
+              src="https://res.cloudinary.com/de7tyskql/image/upload/f_auto,q_auto/zesz5qfddzmw0j1txpdj"
               alt="Delicious Food"
             />
           </div>
